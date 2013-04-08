a6c653cf81fb35b5f032bd92dd9ebdf7c4fc79f3
Derek Remund <ddremund@gmail.com>
2013-04-08 11:07:34 -0500

    Broke server choice out into separate function

--- challenge2.py
+++ challenge2.py
@@ -4,7 +4,24 @@ import pyrax
 import argparse
 import os
 
+def chooseServer(cs, prompt):
 
+    servers = cs.servers.list()
+    
+    srv_dict = {}
+    
+    for pos, srv in enumerate(servers):
+        print "{}: {}".format(pos, srv.name)
+        srv_dict[str(pos)] = (srv.id, srv.name)
+        
+    choice = None
+    
+    while choice not in srv_dict:
+        if choice is not None:
+            print "  ** Not a valid server choice ** "
+        choice = raw_input("Choose a server to clone: ")
+
+    return srv_dict[choice]
 
 
 def main():
@@ -21,26 +38,13 @@ def main():
     pyrax.set_credential_file(creds_file)
     
     cs = pyrax.cloudservers
-    
-    servers = cs.servers.list()
-    
-    srv_dict = {}
-    
-    for pos, srv in enumerate(servers):
-        print "{}: {}".format(pos, srv.name)
-        srv_dict[str(pos)] = (srv.id, srv.name)
-        
-    choice = None
-    
-    while choice not in srv_dict:
-        if choice is not None:
-            print "  ** Not a valid server choice ** "
-        choice = raw_input("Choose a server to clone: ")
 
-    base_id = srv_dict[choice][0]
+    base_server = chooseServer(cs, "Choose a server to clone: ")
+
+    base_id = base_server[0]
     print
 
-    def_image_name = "{}{}".format(srv_dict[choice][1], "-image")
+    def_image_name = "{}{}".format(base_server[1], "-image")
     image_name = raw_input("Enter a name for the image [{}]: ".format(def_image_name))
     if (image_name == ""):
         image_name = def_image_name
